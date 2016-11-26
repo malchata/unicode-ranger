0e5bbfe6ca1331512efc0e5f65f4d476c9b89e47
Jeremy Wagner <jeremy.l.wagner@gmail.com>
2016-11-26 14:55:48 -0600

    Added prepublish step to package.json

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 "use strict";
 
 import unicodeRanger from "./unicode-ranger";
-unicodeRanger.getRange("http://www.soularchive.jp/tmr/archives.htm;https://jeremywagner.me;https://en.wikipedia.org/wiki/Wasp").then((data){
+unicodeRanger.getRange("http://www.soularchive.jp/tmr/archives.htm;https://jeremywagner.me;https://en.wikipedia.org/wiki/Wasp").then((data) => {
 	console.log(ranges)
 });
